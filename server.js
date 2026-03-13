7ae898715fae4de311b90ecdfd759cb146139814
unknown <jmill3142@westada.org>
2026-03-13 14:24:57 +0000

    jack

--- server.js
+++ server.js
@@ -15,3 +15,14 @@ app.get('/about', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`)
 })
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html')
+})
+
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/public/about.html')
+})
+
+app.get('/contact', (req, res) => {
+  res.sendFile(__dirname + '/public/contact.html')
+})
