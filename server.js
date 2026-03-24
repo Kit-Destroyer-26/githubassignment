8ec304857447ce05b917f30cee793df2c656dda2
unknown <jmill3142@westada.org>
2026-03-24 14:45:14 +0000

    changing somethin

--- server.js
+++ server.js
@@ -20,3 +20,6 @@ app.get('/contact', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`)
 })
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
